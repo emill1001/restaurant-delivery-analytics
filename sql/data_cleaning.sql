604c002e10b58529782e44f730de5804929443c7
emill1001 <emilazimov06@gmail.com>
2026-06-30 10:29:04 +0400

    data_cleaning.sql

--- sql/data_cleaning.sql
+++ sql/data_cleaning.sql
@@ -1,1 +1,200 @@
+USE pizza_runner_database
 
+
+DROP TABLE IF EXISTS runners;
+CREATE TABLE runners (
+  runner_id INTEGER,
+  registration_date DATE
+);
+INSERT INTO runners
+  (runner_id, registration_date)
+VALUES
+  (1, '2021-01-01'),
+  (2, '2021-01-03'),
+  (3, '2021-01-08'),
+  (4, '2021-01-15');
+
+
+DROP TABLE IF EXISTS customer_orders;
+CREATE TABLE customer_orders (
+  order_id INTEGER,
+  customer_id INTEGER,
+  pizza_id INTEGER,
+  exclusions VARCHAR(4),
+  extras VARCHAR(4),
+  order_time TIMESTAMP
+);
+
+INSERT INTO customer_orders
+  (order_id, customer_id, pizza_id, exclusions, extras, order_time)
+VALUES
+  ('1', '101', '1', '', '', '2020-01-01 18:05:02'),
+  ('2', '101', '1', '', '', '2020-01-01 19:00:52'),
+  ('3', '102', '1', '', '', '2020-01-02 23:51:23'),
+  ('3', '102', '2', '', NULL, '2020-01-02 23:51:23'),
+  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
+  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
+  ('4', '103', '2', '4', '', '2020-01-04 13:23:46'),
+  ('5', '104', '1', 'null', '1', '2020-01-08 21:00:29'),
+  ('6', '101', '2', 'null', 'null', '2020-01-08 21:03:13'),
+  ('7', '105', '2', 'null', '1', '2020-01-08 21:20:29'),
+  ('8', '102', '1', 'null', 'null', '2020-01-09 23:54:33'),
+  ('9', '103', '1', '4', '1, 5', '2020-01-10 11:22:59'),
+  ('10', '104', '1', 'null', 'null', '2020-01-11 18:34:49'),
+  ('10', '104', '1', '2, 6', '1, 4', '2020-01-11 18:34:49');
+
+
+DROP TABLE IF EXISTS runner_orders;
+CREATE TABLE runner_orders (
+  order_id INTEGER,
+  runner_id INTEGER,
+  pickup_time VARCHAR(19),
+  distance VARCHAR(7),
+  duration VARCHAR(10),
+  cancellation VARCHAR(23)
+);
+
+INSERT INTO runner_orders
+  (order_id, runner_id, pickup_time, distance, duration, cancellation)
+VALUES
+  ('1', '1', '2020-01-01 18:15:34', '20km', '32 minutes', ''),
+  ('2', '1', '2020-01-01 19:10:54', '20km', '27 minutes', ''),
+  ('3', '1', '2020-01-03 00:12:37', '13.4km', '20 mins', NULL),
+  ('4', '2', '2020-01-04 13:53:03', '23.4', '40', NULL),
+  ('5', '3', '2020-01-08 21:10:57', '10', '15', NULL),
+  ('6', '3', 'null', 'null', 'null', 'Restaurant Cancellation'),
+  ('7', '2', '2020-01-08 21:30:45', '25km', '25mins', 'null'),
+  ('8', '2', '2020-01-10 00:15:02', '23.4 km', '15 minute', 'null'),
+  ('9', '2', 'null', 'null', 'null', 'Customer Cancellation'),
+  ('10', '1', '2020-01-11 18:50:20', '10km', '10minutes', 'null');
+
+
+DROP TABLE IF EXISTS pizza_names;
+CREATE TABLE pizza_names (
+  pizza_id INTEGER,
+  pizza_name TEXT
+);
+INSERT INTO pizza_names
+  (pizza_id, pizza_name)
+VALUES
+  (1, 'Meatlovers'),
+  (2, 'Vegetarian');
+
+
+DROP TABLE IF EXISTS pizza_recipes;
+CREATE TABLE pizza_recipes (
+  pizza_id INTEGER,
+  toppings TEXT
+);
+INSERT INTO pizza_recipes
+  (pizza_id, toppings)
+VALUES
+  (1, '1, 2, 3, 4, 5, 6, 8, 10'),
+  (2, '4, 6, 7, 9, 11, 12');
+
+
+DROP TABLE IF EXISTS pizza_toppings;
+CREATE TABLE pizza_toppings (
+  topping_id INTEGER,
+  topping_name TEXT
+);
+INSERT INTO pizza_toppings
+  (topping_id, topping_name)
+VALUES
+  (1, 'Bacon'),
+  (2, 'BBQ Sauce'),
+  (3, 'Beef'),
+  (4, 'Cheese'),
+  (5, 'Chicken'),
+  (6, 'Mushrooms'),
+  (7, 'Onions'),
+  (8, 'Pepperoni'),
+  (9, 'Peppers'),
+  (10, 'Salami'),
+  (11, 'Tomatoes'),
+  (12, 'Tomato Sauce');
+
+-- cleaning customer_orders table
+
+UPDATE customer_orders
+set exclusions = NULL
+WHERE exclusions = '' OR exclusions = 'null';
+
+
+UPDATE customer_orders
+SET extras = NULL
+WHERE extras = '' OR extras = 'null';
+
+-- cleaning runner_orders table
+
+UPDATE runner_orders
+SET pickup_time = NULL
+WHERE pickup_time = 'null';
+
+ALTER TABLE runner_orders
+MODIFY COLUMN pickup_time timestamp;
+
+
+-- changing distance column
+
+UPDATE runner_orders
+set distance = CASE
+WHEN distance LIKE '%null%' THEN NULL
+WHEN distance LIKE '%km%' THEN trim(replace(distance, 'km', ''))
+ELSE trim(distance)
+end;
+
+ALTER TABLE runner_orders
+modify COLUMN distance decimal(4,1);
+
+-- changing duration column
+
+UPDATE runner_orders
+SET duration = CASE
+WHEN duration LIKE '%null%' THEN NULL
+WHEN duration LIKE '%mins%' THEN trim(replace(duration, 'mins', ''))
+WHEN duration LIKE '%minutes%' then trim(replace(duration, 'minutes', ''))
+WHEN duration LIKE '%minute%' THEN trim(replace(duration, 'minute', ''))
+ELSE trim(duration)
+end;
+
+ALTER TABLE runner_orders
+MODIFY COLUMN duration integer;
+
+UPDATE runner_orders
+SET duration = sec_to_time(duration * 60)
+WHERE duration IS NOT NULL;
+
+ALTER TABLE runner_orders
+MODIFY COLUMN duration time;
+
+-- cancellation column
+
+UPDATE runner_orders
+SET cancellation = NULL
+WHERE cancellation = 'null';
+
+UPDATE runner_orders
+SET cancellation = NULL
+WHERE cancellation = '';
+
+-- changing the format of toppings from text to an integer
+
+CREATE TABLE pizza_recipes_normalized(
+pizza_id int,
+topping_id int);
+
+INSERT INTO pizza_recipes_normalized(pizza_id, topping_id)
+values(1,1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 8), (1, 10), 
+(2, 4), (2, 6), (2, 7), (2, 9), (2, 11), (2, 12);
+
+-- making more space for exclusions and extras
+
+ALTER TABLE customer_orders
+MODIFY COLUMN exclusions varchar(30);
+
+ALTER TABLE customer_orders
+MODIFY COLUMN extras varchar(30);
+
+
+-- preprocessing is over
